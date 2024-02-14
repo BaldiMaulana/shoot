639e8373472e786f2abfed993bf235983ab3ae44
BaldiMaulana <m.baldimaulana.com>
2024-02-15 05:13:02 +0700

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Target from '@/app/target/page'
 export default function Home() {
   return (
-    <main className='sm:flex justify-center items-center h-screen bg-gradient-to-r from-gray-900 to-slate-950 text-white font-poppins'>
+    <main className='flex justify-center items-center h-screen bg-gradient-to-r from-gray-900 to-slate-950 text-white font-poppins'>
       <Target/>
     </main>
   );
